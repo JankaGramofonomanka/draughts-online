d607145c1a282c6e4f76caa694057fc0fa4184b1
JankaGramofonomanka <tsrn666@gmail.com>
2021-05-29 19:39:57 +0200

    Add validation of piece positions
    Defined functions for validating positions of pieces in the game.
    Defined the `Error` type as an alias for `String`.

--- src/GameState.hs
+++ src/GameState.hs
@@ -4,7 +4,16 @@ module GameState where
 
 import qualified Data.Map as M
 
+import Control.Monad.Except
 import Control.Monad.State.Strict
+
+
+
+-- make an alias in case we want more sophisticated errors in the future
+type Error = String
+mkError :: String -> Error
+mkError = id
+
 
 
 -- `GameState` data definition
@@ -51,6 +60,36 @@ getHeight = do
   return height
 
 
+
+-- validation of piece positions
+onBoard :: MonadState GameState m => Pos -> m Bool
+onBoard (x, y) = do
+  (width, height) <- getDimension
+  return $ 0 <= x && x < width && 0 <= y && y < height
+
+posColor :: Pos -> Color
+posColor (x, y) = if x `mod` 2 == y `mod` 2 then Black else White
+
+
+validateState :: (MonadState GameState m, MonadError Error m) =>
+  m ()
+validateState = do
+  board <- getBoard
+  stateIsValid <- foldl foldFunc (pure True) (M.toList board)
+
+  if stateIsValid then
+    return ()
+  
+  else
+    throwError $ mkError "state is invalid"
+
+  where 
+    foldFunc :: MonadState GameState m => m Bool -> (Piece, Pos) -> m Bool
+    foldFunc pureAcc (piece, pos) = do
+      acc <- pureAcc
+      posOnBoard <- onBoard pos
+      let colorMatchesPos = color piece == posColor pos
+      return $ acc && posOnBoard && colorMatchesPos
 
 
 
