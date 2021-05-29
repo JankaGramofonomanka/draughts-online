b77c8528ac2f9f69ece935a1a2912e6d32abce71
JankaGramofonomanka <tsrn666@gmail.com>
2021-05-29 19:36:11 +0200

    Define get functions for `GameState`

--- src/GameState.hs
+++ src/GameState.hs
@@ -1,7 +1,13 @@
+{-# LANGUAGE FlexibleContexts #-}
+
 module GameState where
 
 import qualified Data.Map as M
 
+import Control.Monad.State.Strict
+
+
+-- `GameState` data definition
 data Color = Black | White deriving (Ord, Eq, Show, Read)
 type Piece = (Color, Int)
 color :: Piece -> Color
@@ -19,4 +25,32 @@ data GameState = GameState {
 
 emptyState :: Int -> Int -> GameState
 emptyState w h = GameState {board = M.empty, dimension = (w, h)}
+
+
+
+-- get functions
+getBoard :: MonadState GameState m => m Board
+getBoard = do
+  state <- get
+  return $ board state  
+
+
+getDimension :: MonadState GameState m => m (Int, Int)
+getDimension = do
+  state <- get
+  return $ dimension state
+
+getWidth :: MonadState GameState m => m Int
+getWidth = do
+  (width, height) <- getDimension
+  return width
+
+getHeight :: MonadState GameState m => m Int
+getHeight = do
+  (width, height) <- getDimension
+  return height
+
+
+
+
 
