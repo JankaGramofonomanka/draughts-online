3bbb80cc77f0238f5408fe6c129191b0ffe4c534
JankaGramofonomanka <tsrn666@gmail.com>
2021-06-11 13:20:47 +0200

    Add basic graphics to the client

--- client/Main.hs
+++ client/Main.hs
@@ -1,6 +1,39 @@
 module Main where
 
-import Lib
+import Brick
+import Brick.Widgets.Center
+import Brick.Widgets.Border
+import Brick.Widgets.Border.Style
+import Graphics.Vty as V
+
+
+
+
+
+myWidget :: Widget ()
+myWidget = withBorderStyle unicode 
+  $ borderWithLabel (str "rectangle")
+  $ hCenter
+  $ padAll 1
+  $ str "RECTANGLE"
+
+
+
+
+handleEvent appState (VtyEvent (V.EvKey V.KEsc [])) = halt appState 
+handleEvent appState _ = continue appState 
+
+theMap :: AttrMap
+theMap = attrMap V.defAttr []
+
+app :: App () () ()
+app = App { appDraw = const [myWidget]
+          , appChooseCursor = neverShowCursor
+          , appHandleEvent = handleEvent
+          , appStartEvent = return
+          , appAttrMap = const theMap
+          }
 
 main :: IO ()
-main = someFunc
+main = do
+  defaultMain app ()
