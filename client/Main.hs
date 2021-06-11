a15fe56f2b9e88d731b38e2cb11405a31fca80eb
JankaGramofonomanka <tsrn666@gmail.com>
2021-06-11 14:28:17 +0200

    Draw move buttons

--- client/Main.hs
+++ client/Main.hs
@@ -6,8 +6,9 @@ import Brick.Widgets.Border
 import Brick.Widgets.Border.Style
 import Graphics.Vty as V
 
+import qualified GameState as GS
 
-
+type AppState = ()
 
 
 myWidget :: Widget ()
@@ -17,6 +18,20 @@ myWidget = withBorderStyle unicode
   $ padAll 1
   $ str "RECTANGLE"
 
+dirWidget :: GS.Direction -> Widget ()
+dirWidget dir = withBorderStyle unicode
+  $ borderWithLabel (str "")
+  $ hCenter
+  $ str $ show dir
+
+drawDirs :: Widget ()
+drawDirs = 
+      (dirWidget GS.TopLeft <+> dirWidget GS.TopRight)
+  <=> (dirWidget GS.BotLeft <+> dirWidget GS.BotRight)
+
+drawApp :: AppState -> [Widget ()]
+drawApp _ = return $
+  drawDirs
 
 
 
@@ -26,8 +41,8 @@ handleEvent appState _ = continue appState
 theMap :: AttrMap
 theMap = attrMap V.defAttr []
 
-app :: App () () ()
-app = App { appDraw = const [myWidget]
+app :: App AppState () ()
+app = App { appDraw = drawApp
           , appChooseCursor = neverShowCursor
           , appHandleEvent = handleEvent
           , appStartEvent = return
